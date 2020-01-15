7297337cbc49b9671889413dbb3da77ad5b28ae7
Elvin Peng <elvinpeng@tencent.com>
2020-01-15 16:36:14 +0800

    feat: init project

---
+++ .eslintrc.js
@@ -0,0 +1,12 @@
+module.exports = {
+  parser: '@typescript-eslint/parser',
+  extends: [
+    'plugin:@typescript-eslint/recommended',
+    'airbnb-base',
+  ],
+  parserOptions: {
+    ecmaVersion: 2018,
+    sourceType: 'module',
+  },
+  rules: {},
+};
---
+++ README.md
@@ -0,0 +1,7 @@
+# my-blog
+
+A personal blog project. Still in development.
+
+## Thanks List
+
+1. [TypeScript-Node-Starter](https://github.com/microsoft/TypeScript-Node-Starter)
